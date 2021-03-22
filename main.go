a89be8a7c8927aa350e4187828f585143bbc9a5d
Administrator <admin@example.com>
2021-03-22 09:37:17 +0000

    Update main.go

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import "net/http"
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("hello world!test 11111 22222"))
+		w.Write([]byte("hello world!test 11111 22222 3333"))
 	})
 
 	http.ListenAndServe(":8000", nil)
