8b731318a27c65d0616d89eac405a51b01358d24
zhangzhen <zhangzhen@rp-pet.cn>
2021-03-22 18:27:50 +0800

    提交

--- main.go
+++ main.go
@@ -6,6 +6,5 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("hello world!test 11111 22222 3333"))
 	})
-
 	http.ListenAndServe(":8000", nil)
 }
